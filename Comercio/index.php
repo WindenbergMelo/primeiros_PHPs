150e6bd1f9e400cf3d4427a9b40fc2a898e7d649
José Windenberg Andrade de Melo <windenbergandrade@gmail.com>
2022-12-08 16:27:54 -0300

    PG Home
    modelo de pagina inicial

--- Comercio/index.php
+++ Comercio/index.php
@@ -1,34 +1,32 @@
 <html lang="PT-BR">
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>Comercio</title>
     </head>
     <body>
-        <?php
         
-            require_once 'classes/Cliente.php';
-            require_once 'classes/Vendedor.php';
-            require_once 'classes/Produto.php';
-            require_once 'classes/Carrinho.php';
-            require_once './classes/Caixa.php';
-            
-            $produto[0] = new Produto(/*nome*/"Macarrao",/*valor*/2.50,/*quantidade*/ 15,/*codigo de barra*/ 2002);
-            $produto[1] = new Produto("Arroz", 3.00, 20, 2003);
-            $produto[2] = new Produto("Feijão", 3.50, 20, 2004);
-            
-            $vendedor[0] = new Vendedor(/*nome*/"Bibi", /*cpf*/2345, /*salario*/50.8);
-            $cliente[0] = new Cliente(/*nome*/"Berg", /*cpf*/1234, /*cep*/39535000,/*rua*/ "pico do cabugi",/*numero*/ 264, /*saldo*/50);   
-            
-            $carrinho[0] = new Carrinho();
-            
-            $carrinho[0]->addCarrinho($produto[0], 10, $produto[1], 4, $produto[2], 5);
-            
-            $carrinho[0]->NotaFiscal();
-            
-            $caixa[0] = new Caixa($cliente[0], $vendedor[0], $carrinho[0], "A vista");
-            
-            $cliente[0]->pagarDivida(50);
-            
-        ?>
+        <header>
+            <nav>
+                <table>
+                    
+                </table>
+            </nav>
+        </header>
+
+        <div>
+            <article>
+                <section>
+
+                </section>
+            </article>
+
+            <aside>
+
+            </aside>
+        </div>
+        <footer>
+
+        </footer>
+
     </body>
 </html>
